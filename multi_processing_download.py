5d9f2f2397d2c061dfabd25ba7c07f2e27ea7e7a
JanUrb <JanUrb@users.noreply.github.com>
2016-08-09 12:23:42 +0200

    clean up

--- multi_processing_download.py
+++ multi_processing_download.py
@@ -11,21 +11,20 @@ def _download_and_save_file(url, file_path):
     r = requests.get(url, stream=True)
     with open(file_path, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
     return r.status_code
+
 
 def _mp_download_wrapper(url_item):
     query, _, file_path = url_item
     log.debug('Downloading:\t' + query)
     _download_and_save_file(query, file_path)
 
+
 def init_download(url_items, nr_of_processes=4):
     p = multiprocessing.Pool(nr_of_processes)
     p.map(_mp_download_wrapper, url_items)
-
-
-
 
 
 if __name__ == '__main__':
